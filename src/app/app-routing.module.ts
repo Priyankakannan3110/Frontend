2225ace1ce86716712a88db7d9af4ca594200aa5
Priyanka <priyankakannan3110@gmail.com>
2022-04-07 14:14:43 +0530

    initial code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'signup',component:SignupComponent},
+  {path:'login', component:LoginComponent},
+  {path:'',redirectTo:'\login',pathMatch:'full'},
+  {path:'admin',loadChildren:()=>import('./modules/admin/admin.module').then((m)=>m.AdminModule)},
+  {path:'user',loadChildren:()=>import('./modules/user/user.module').then((m)=>m.UserModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
