1fc11ca8b55b972836aa8c6f3f0de7933bad1210
Priyanka <priyankakannan3110@gmail.com>
2022-04-11 13:30:42 +0530

    appointment update

--- src/app/modules/user/components/add-appointment/add-appointment.component.ts
+++ src/app/modules/user/components/add-appointment/add-appointment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Appointment } from 'src/app/models/appointment';
+import { AppointmentService } from 'src/app/services/appointment.service';
 
 @Component({
   selector: 'app-add-appointment',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-appointment.component.css']
 })
 export class AddAppointmentComponent implements OnInit {
-
-  constructor() { }
+  
+  appointment:Appointment=new Appointment();
+  constructor(private appointmentService:AppointmentService,private router:Router) { }
 
   ngOnInit(): void {
   }
+  saveAppointment(){
+    this.appointmentService.createAppointment(this.appointment).subscribe( data =>{
+      console.log(data);
+      //this.goToAppointmentList();
+    },
+    error => console.log(error));
+  }
 
+  goToAppointmentList(){
+    this.router.navigate(['/my-bookings']);
+  }
+  
+  onSubmit(){
+    console.log(this.appointment);
+    this.saveAppointment();
+  }
 }
--- src/app/modules/user/components/my-bookings/my-bookings.component.ts
+++ src/app/modules/user/components/my-bookings/my-bookings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Appointment } from 'src/app/models/appointment';
+import { AppointmentService } from 'src/app/services/appointment.service';
 
 @Component({
   selector: 'app-my-bookings',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyBookingsComponent implements OnInit {
 
-  constructor() { }
+  appointments!:Appointment[];
+  constructor(private appointmentService:AppointmentService,private router:Router) { }
 
   ngOnInit(): void {
+    this.getAppointments();
   }
+  private getAppointments() {
+    this.appointmentService.getAppointmentsList().subscribe(data=>{
+      this.appointments=data;
+    });
+  }
+  appointmentDetails(id: number) {
+    this.router.navigate(['my-bookings', id]);
+ }
+ updateAppointment(id: number) {
+   this.router.navigate(['update-appointment', id ]);
+}
+  deleteAppointment(id: number) {
+   this.appointmentService.deleteAppointment(id).subscribe(data => {
+     alert("Successfully deleted")
+      console.log(data);
+     this.getAppointments();
+    },error=>alert("Sorry "));
+ }
+ 
+
 
 }
